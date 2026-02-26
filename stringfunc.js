b45a83d708300946d04ff26ad24ae745418025e7
Sudipta <maitisudipta02@gmail.com>
2026-02-26 15:04:09 +0530

    commit a new file

---
+++ stringfunc.js
@@ -0,0 +1,11 @@
+let text = "Barak Obama";
+
+console.log(text.length);
+console.log(text.charAt(4));
+// UTF-16 code
+console.log(text.charCodeAt(2));
+console.log(text.codePointAt(2));
+// substr- deprecated
+console.log(text.substr(1,2));
+// subdtring
+console.log(text.substring(1,2));
